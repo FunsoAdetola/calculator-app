c880e0bc4d50bef6b142ebc5447acd745375d694
Funsoadetola <{funsoadetola}@gmail.com>
2020-12-06 00:07:02 +0100

    Updates

---
+++ precache-manifest.e2aeb7b66209d133b3523b13a0c18d93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a59a206c0b437eb907f46697db40aa9",
+    "url": "/calculator-app/index.html"
+  },
+  {
+    "revision": "ce73da8517c230c55dfe",
+    "url": "/calculator-app/static/css/main.ab116635.chunk.css"
+  },
+  {
+    "revision": "1c6ef17f05bbef9f1ca9",
+    "url": "/calculator-app/static/js/2.765c6aaa.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator-app/static/js/2.765c6aaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce73da8517c230c55dfe",
+    "url": "/calculator-app/static/js/main.b85475df.chunk.js"
+  },
+  {
+    "revision": "e1116a6a3b4d51df0274",
+    "url": "/calculator-app/static/js/runtime-main.e00dc3da.js"
+  }
+]);
